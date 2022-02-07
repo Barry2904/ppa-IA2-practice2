7cfa60dd7615fd3ec44404e66e3f28da5c778f19
Bharath J Ramalingam <96277696+Barry2904@users.noreply.github.com>
2022-02-07 09:26:46 +0000

    p2 done

--- p2original.c
+++ p2original.c
@@ -1,3 +1,4 @@
+/*
 #include<stdio.h>
 int input_side()
 {
@@ -38,4 +39,36 @@ int main()
   result=check_scalene(a,b,c);
   output(a,b,c,result);
   return 0;
+}
+*/
+
+#include<stdio.h>
+int input_side(){
+  int n;
+  printf("enter the three sides->");
+  scanf("%d",&n);
+  return n;
+}
+int check_scelene(int a, int b,int c){
+  int isscalene;
+  if (a!=b && a!=c && b!=c && c!=b){
+    isscalene=0;
+    
+  }
+  return isscalene;
+
+
+}
+void output(int a,int b,int c,int isscalene ){
+  printf("the sides of the traingle is scelne %d%d%d%d",a,b,c,isscalene);
+}
+int main(){
+  int a,b,c,res;
+  a=input_side();
+  b=input_side();
+  c=input_side();
+  res=check_scelene(a,b,c);
+  output(a,b,c,res);
+  return 0;
+
 }
