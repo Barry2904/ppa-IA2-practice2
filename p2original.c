df4801211080bb88c1d236a0135fa3141be780e0
Bharath J Ramalingam <96277696+Barry2904@users.noreply.github.com>
2022-02-08 00:28:43 +0000

    p2 done

--- p2original.c
+++ p2original.c
@@ -1,33 +1,33 @@
-/*
+
 #include<stdio.h>
 int input_side()
 {
   int x;
   printf("Enter side length:- ");
   scanf("%d",&x);
+  return x;
 }
 int check_scalene(int a,int b,int c)
 {
-  if(a==b && b==c && c==a)
+  if(a!=b && a!=c && b!=c)
   {
-    int x=0;
-    return x ;
+    return 1;
   }
-  else
+  else 
   {
-    int x=1;
-    return x;
+    return 0;
   }
+  
 }
 void output(int a,int b,int c,int isscalene)
 {
-  if(isscalene!=0)
+  if(isscalene==0)
   {
     printf("Triangle is not scalene");
   }
-  else if(isscalene==0)
+  else if(isscalene==1)
   {
-    printf("is a scalene triangle measurement");
+    printf("Triangle is scalene");
   }
 }
 int main()
@@ -40,35 +40,4 @@ int main()
   output(a,b,c,result);
   return 0;
 }
-*/
 
-#include<stdio.h>
-int input_side(){
-  int n;
-  printf("enter the three sides->");
-  scanf("%d",&n);
-  return n;
-}
-int check_scelene(int a, int b,int c){
-  int isscalene;
-  if (a!=b && a!=c && b!=c && c!=b){
-    isscalene=0;
-    
-  }
-  return isscalene;
-
-
-}
-void output(int a,int b,int c,int isscalene ){
-  printf("the sides of the traingle is scelne %d%d%d%d",a,b,c,isscalene);
-}
-int main(){
-  int a,b,c,res;
-  a=input_side();
-  b=input_side();
-  c=input_side();
-  res=check_scelene(a,b,c);
-  output(a,b,c,res);
-  return 0;
-
-}
